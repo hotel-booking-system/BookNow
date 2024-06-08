8c64d81d19608e707b9ca81bc99c2a5f1c316cbf
emillyQueiroz <eliada.estudos@gmail.com>
2024-06-08 01:03:37 -0300

    Atualizando typescript

--- book-now/src/app/Components/hotel/hotel-register/hotel-register.component.ts
+++ book-now/src/app/Components/hotel/hotel-register/hotel-register.component.ts
@@ -1,12 +1,73 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, FormGroup, FormControl, Validators, NG_VALIDATORS } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HotelService } from '../../../service/hotel.service';
+import { Hotel } from '../../../models/hotel.models';
+import { HeaderComponent } from "../../header/header.component";
+
+const hotelForm = new FormGroup({
+  'title': new FormControl(),
+  'email': new FormControl(),
+  'password': new FormControl()
+});
 
 @Component({
   selector: 'app-hotel-register',
   standalone: true,
-  imports: [],
   templateUrl: './hotel-register.component.html',
-  styleUrl: './hotel-register.component.css'
+  styleUrl: './hotel-register.component.css',
+  imports: [
+    HeaderComponent,
+    FormsModule,
+    RouterModule,
+    HttpClientModule
+  ]
 })
-export class HotelRegisterComponent {
+export class HotelRegisterComponent implements OnInit {
+
+  hotel = new Hotel();
+
+  hotelForm!: FormGroup;
+
+  constructor(private hotelService: HotelService) {
+
+  }
+  onFileChange(event: any): void {
+    this.hotel.images = event.target.files; // Captura os arquivos de imagem selecionados pelo usuário
+  }
+
+  ngOnInit(): void {
+    this.hotelForm = new FormGroup({
+      title: new FormControl(this.hotel.title, [
+        Validators.required,
+        Validators.minLength(3)
+      ])
+    });
+  }
+
+  get title() {
+    return this.hotelForm.get('title');
+  }
+
+  onSubmit() {
+    console.log('Hotel cadastrado: ', this.hotel);
+  }
+
+  registerHotel(): void {
+    // chamar o método do serviço para cadatrar o usuário
+    this.hotelService.registerHotel(this.hotel).subscribe({
+      next: (response) => {
+        // lógica para tratar a resposta do servidor (ex.: exibir msg de sucesso)
+        console.log('Acomodação cadastrada: ', response);
+      },
+      error: (error) => {
+        // lógica para tratar erros (ex.: exibir msg de erro)
+        console.error('Erro ao cadastrar acomodação: ', error);
+      }
+    })
+  }
 
 }
+
--- book-now/src/app/models/hotel.models.ts
+++ book-now/src/app/models/hotel.models.ts
@@ -1,25 +1,24 @@
 export class Hotel {
+  public id: number;
+  public title: string;
+  public location: string;
+  public description: string;
+  public price: string;
+  public images: File[]; // Adicionando uma nova propriedade para os arquivos de imagem
 
-    public id: number;
-    public name: string;
-    public location : string;
-    public price: string;
-    public rating : number;
-  
-    constructor(
-      id?: number,
-      name?: string,
-      location?: string,
-      price?: string,
-      rating?: number){
-  
-        this.id = id || 0;
-        this.name = name || '';
-        this.location = location || '';
-        this.price = price || '';
-        this.rating = rating || 0;
-  
-    }
-  
+  constructor(
+    id?: number,
+    title?: string,
+    location?: string,
+    description?: string,
+    price?: string,
+    images?: File[] // Adicionando os arquivos de imagem ao construtor
+  ) {
+    this.id = id || 0;
+    this.title = title || '';
+    this.location = location || '';
+    this.description = description || '';
+    this.price = price || '';
+    this.images = images || []; // Inicializando os arquivos de imagem
   }
-  
+}
